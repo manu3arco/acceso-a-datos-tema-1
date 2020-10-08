c1d60bf0bb3841a3eaaf47b1808064b0214e4b0c
= <56394830+manu3arco@users.noreply.github.com>
2020-10-08 08:37:27 +0200

    contadorpalabras

--- src/clases/contapala.java
+++ src/clases/contapala.java
@@ -11,10 +11,14 @@ public class contapala {
 
         int contador_a = 0, contador_e = 0, contador_i = 0, contador_o = 0, contador_u = 0;
 
-        char  cont_a ='a';
+        String cuenta_a = null;
+        String cuenta_e = null;
+        String cuenta_i = null;
+        String cuenta_o = null;
+        String cuenta_u = null;
 
         marco.setSize(500, 400);
-        marco.setLayout(new GridLayout(2,1));
+        marco.setLayout(new GridLayout(6,1));
 
 
         JPanel panel1 = new JPanel();
@@ -23,6 +27,17 @@ public class contapala {
         JPanel panel2 = new JPanel();
         panel2.setLayout(new FlowLayout(FlowLayout.CENTER));
 
+        JPanel panel3 = new JPanel();
+        panel3.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        JPanel panel4 = new JPanel();
+        panel4.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        JPanel panel5 = new JPanel();
+        panel5.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        JPanel panel6 = new JPanel();
+        panel6.setLayout(new FlowLayout(FlowLayout.CENTER));
 
 
         JLabel nombre = new JLabel("Número de palabras: ");
@@ -39,6 +54,34 @@ public class contapala {
         panel2.add(letra_a);
         panel2.add(contar_a);
 
+        JLabel letra_e = new JLabel("Número de vocales e: ");
+        JTextField contar_e = new JTextField();
+        contar_e.setPreferredSize( new Dimension( 50, 24 ) );
+        contar_e.setEditable(false);
+        panel3.add(letra_e);
+        panel3.add(contar_e);
+
+        JLabel letra_i = new JLabel("Número de vocales i: ");
+        JTextField contar_i = new JTextField();
+        contar_i.setPreferredSize( new Dimension( 50, 24 ) );
+        contar_i.setEditable(false);
+        panel4.add(letra_i);
+        panel4.add(contar_i);
+
+        JLabel letra_o = new JLabel("Número de vocales o: ");
+        JTextField contar_o = new JTextField();
+        contar_o.setPreferredSize( new Dimension( 50, 24 ) );
+        contar_o.setEditable(false);
+        panel5.add(letra_o);
+        panel5.add(contar_o);
+
+        JLabel letra_u = new JLabel("Número de vocales u: ");
+        JTextField contar_u = new JTextField();
+        contar_u.setPreferredSize( new Dimension( 50, 24 ) );
+        contar_u.setEditable(false);
+        panel6.add(letra_u);
+        panel6.add(contar_u);
+
                 try {
                     File f = new File(String.valueOf(fc.getSelectedFile()));
                     String linea;
@@ -54,10 +97,6 @@ public class contapala {
                                 if (linea.charAt(i - 1) == ' ')
                                     if (linea.charAt(i) != ' ')
                                         a++;
-
-                               /* for ( i = 0; i < linea.length(); i++) {
-                                        contador_a++;
-                                    }*/
                             }
                         }
                     }
@@ -68,9 +107,53 @@ public class contapala {
                     ew.printStackTrace();
                 }
 
+                try {
+                    File f1 = new File(String.valueOf(fc.getSelectedFile()));
+                    FileReader fr1 = new FileReader(f1);
+                    BufferedReader br1 = new BufferedReader(fr1);
+                    String lineaa;
+                    while ((lineaa = br1.readLine()) != null) {
+                        for (int x = 0; x < lineaa.length(); x++) {
+                            if (lineaa.charAt(x) == 'a') {
+                                contador_a++;
+                                cuenta_a = Integer.toString(contador_a);
+                            }
+                            if (lineaa.charAt(x) == 'e') {
+                                contador_e++;
+                                cuenta_e = Integer.toString(contador_e);
+                            }
+                            if (lineaa.charAt(x) == 'i') {
+                                contador_i++;
+                                cuenta_i = Integer.toString(contador_i);
+                            }
+                            if (lineaa.charAt(x) == 'o') {
+                                contador_o++;
+                                cuenta_o = Integer.toString(contador_o);
+                            }
+                            if (lineaa.charAt(x) == 'u') {
+                                contador_u++;
+                                cuenta_u = Integer.toString(contador_u);
+                            }
+                        }
+                    }
+                    } catch(FileNotFoundException ae){
+                        ae.printStackTrace();
+                    } catch(IOException we) {
+                    we.printStackTrace();
+                }
+
+        contar_a.setText(cuenta_a);
+        contar_e.setText(cuenta_e);
+        contar_i.setText(cuenta_i);
+        contar_o.setText(cuenta_o);
+        contar_u.setText(cuenta_u);
+
         marco.add(panel1);
         marco.add(panel2);
-
+        marco.add(panel3);
+        marco.add(panel4);
+        marco.add(panel5);
+        marco.add(panel6);
         marco.setVisible(true);
 
     }
