b2375ce3bd750e6f0068f20014030d9118fef431
Carlos Asís <casisanton@safareyes.es>
2020-10-09 08:25:58 +0200

    terminado copiarchivo

--- src/clases/copiarchi.java
+++ src/clases/copiarchi.java
@@ -2,8 +2,7 @@ package clases;
 
 import javax.swing.*;
 import java.awt.*;
-import java.io.File;
-import java.text.SimpleDateFormat;
+import java.io.*;
 
 public class copiarchi {
     public static void copiar(String s) {
@@ -30,7 +29,10 @@ public class copiarchi {
         JPanel panel5 = new JPanel();
         panel5.setLayout(new FlowLayout(FlowLayout.CENTER));
 
-        JLabel nombre = new JLabel("Nombre del archivo: ");
+        JPanel panel6 = new JPanel();
+        panel6.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+        JLabel nombre = new JLabel("Nombre del fichero: ");
         JTextField texto1 = new JTextField();
         texto1.setPreferredSize( new Dimension( 300, 24 ) );
         texto1.setEditable(false);
@@ -61,15 +63,18 @@ public class copiarchi {
         panel4.add(new JLabel("Ruta de destino: "));
         panel4.add(texto3);
 
+        JTextField texto4 = new JTextField();
+        texto4.setPreferredSize( new Dimension( 200, 24 ));
+        panel5.add(new JLabel("Nuevo nombre del fichero: "));
+        panel5.add(texto4);
 
         JButton añadir = new JButton("Copiar fichero");
-
+        panel6.add(añadir);
 
         file.addActionListener(e -> {
             if (fc.showOpenDialog(null) == JFileChooser.APPROVE_OPTION){
 
-                File f2 = new File(String.valueOf(fc.getSelectedFile()));
-                texto3.setText(f2.getAbsolutePath());
+                texto3.setText(fc.getSelectedFile().getAbsolutePath());
 
 
             }else{
@@ -79,12 +84,44 @@ public class copiarchi {
             }
         });
 
+        añadir.addActionListener(e -> {
+            try {
+                FileReader lector = new FileReader(f.getAbsolutePath());
+                BufferedReader entrada = new BufferedReader(lector);
+                FileWriter escritor = new FileWriter(fc.getSelectedFile().getAbsolutePath()+"\\"+texto4.getText()+".txt");
+                BufferedWriter salida = new BufferedWriter(escritor);
+
+                String cadena = entrada.readLine();
+
+                while (cadena != null) {
+                    salida.write(cadena);
+                    salida.newLine();
+                    cadena = entrada.readLine();
+                }
+
+                entrada.close();
+                salida.close();
+
+                JOptionPane.showMessageDialog(marco,"Se ha copiado el fichero con éxito.",
+                        "Info",
+                        JOptionPane.INFORMATION_MESSAGE);
+
+                salida.close();
+
+            } catch (IOException ioe) {
+                System.err.println("¡Hubo un error de E/S!");
+            }
+        });
+
+
+
 
         marco.add(panel1);
         marco.add(panel2);
         marco.add(panel3);
         marco.add(panel4);
         marco.add(panel5);
+        marco.add(panel6);
 
         marco.setVisible(true);
         marco.setResizable(false);
